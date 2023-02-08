5c031be0907307ec36233cf909af1e8a7fa24cba
Mohd Faheem Uddin <123308056+MohdFaheemUddin@users.noreply.github.com>
2023-02-08 12:10:09 +0530

    Create default.js

---
+++ tests/configs/modules/newsfeed/default.js
@@ -0,0 +1,28 @@
+/* SmartMirror Test config newsfeed module
+ *
+ * By Rodrigo Ramírez Norambuena https://rodrigoramirez.com
+ * MIT Licensed.
+ */
+let config = {
+	timeFormat: 12,
+
+	modules: [
+		{
+			module: "newsfeed",
+			position: "bottom_bar",
+			config: {
+				feeds: [
+					{
+						title: "Rodrigo Ramirez Blog",
+						url: "http://localhost:8080/tests/mocks/newsfeed_test.xml"
+					}
+				]
+			}
+		}
+	]
+};
+
+/*************** DO NOT EDIT THE LINE BELOW ***************/
+if (typeof module !== "undefined") {
+	module.exports = config;
+}
