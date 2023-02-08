5e91412027f43c8e5bde36e6ba0079c3e065c95e
Mohd Faheem Uddin <123308056+MohdFaheemUddin@users.noreply.github.com>
2023-02-08 11:18:05 +0530

    Create fail-basic-auth.js

---
+++ tests/configs/modules/calendar/fail-basic-auth.js
@@ -0,0 +1,35 @@
+/* SmartMirror Test calendar calendar
+ *
+ * This configuration is a wrong authentication
+ *
+ * By Rodrigo Ramírez Norambuena https://rodrigoramirez.com
+ * MIT Licensed.
+ */
+let config = {
+	timeFormat: 12,
+
+	modules: [
+		{
+			module: "calendar",
+			position: "bottom_bar",
+			config: {
+				calendars: [
+					{
+						maximumNumberOfDays: 10000,
+						url: "http://localhost:8020/tests/mocks/calendar_test.ics",
+						auth: {
+							user: "SmartMirror",
+							pass: "Project001",
+							method: "basic"
+						}
+					}
+				]
+			}
+		}
+	]
+};
+
+/*************** DO NOT EDIT THE LINE BELOW ***************/
+if (typeof module !== "undefined") {
+	module.exports = config;
+}
