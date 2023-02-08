b4f187e9000be8b152363eea5ea89b2be47bc4cf
Mohd Faheem Uddin <123308056+MohdFaheemUddin@users.noreply.github.com>
2023-02-08 11:26:11 +0530

    Create clock_12hr.js

---
+++ tests/configs/modules/clock/clock_12hr.js
@@ -0,0 +1,20 @@
+/* SmartMirror Test config for default clock module
+ *
+ * By Sergey Morozov
+ * MIT Licensed.
+ */
+let config = {
+	timeFormat: 12,
+
+	modules: [
+		{
+			module: "clock",
+			position: "middle_center"
+		}
+	]
+};
+
+/*************** DO NOT EDIT THE LINE BELOW ***************/
+if (typeof module !== "undefined") {
+	module.exports = config;
+}
