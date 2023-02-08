4a2f6107f687d6a849d05e60f64821730fae5ac3
Mohd Faheem Uddin <123308056+MohdFaheemUddin@users.noreply.github.com>
2023-02-08 11:12:31 +0530

    Create auth-default.js

---
+++ tests/configs/modules/calendar/auth-default.js
@@ -0,0 +1,32 @@
+/* SmartMirror Test config default calendar with auth by default
+ *
+ * By Rodrigo Ramírez Norambuena https://rodrigoramirez.com
+ * MIT Licensed.
+ */
+let config = {
+	timeFormat: 12,
+
+	modules: [
+		{
+			module: "calendar",
+			position: "bottom_bar",
+			config: {
+				calendars: [
+					{
+						maximumNumberOfDays: 10000,
+						url: "http://localhost:8080/tests/mocks/calendar_test.ics",
+						auth: {
+							user: "SmartMirror",
+							pass: "Project001"
+						}
+					}
+				]
+			}
+		}
+	]
+};
+
+/*************** DO NOT EDIT THE LINE BELOW ***************/
+if (typeof module !== "undefined") {
+	module.exports = config;
+}
