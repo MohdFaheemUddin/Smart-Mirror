4d562dfe41d96382a7bb0bd950a687962f1dfc97
Mohd Faheem Uddin <123308056+MohdFaheemUddin@users.noreply.github.com>
2023-02-08 14:15:23 +0530

    Create utlis.js

---
+++ js/utlis.js
@@ -0,0 +1,14 @@
+/* SmartMIrror
+ * Utils
+ * MIT Licensed.
+ */
+const colors = require("colors/safe");
+
+module.exports = {
+	colors: {
+		warn: colors.yellow,
+		error: colors.red,
+		info: colors.blue,
+		pass: colors.green
+	}
+};
