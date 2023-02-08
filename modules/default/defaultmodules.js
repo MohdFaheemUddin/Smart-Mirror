b3a817f61ab129d12a60866837cd0297d10e8f6e
Mohd Faheem Uddin <123308056+MohdFaheemUddin@users.noreply.github.com>
2023-02-08 14:05:02 +0530

    Create defaultmodules.js

---
+++ modules/default/defaultmodules.js
@@ -0,0 +1,12 @@
+/* SmartMirror Default Modules List
+ * Modules listed below can be loaded without the 'default/' prefix. Omitting the default folder name.
+ *
+ * By Michael Teeuw https://michaelteeuw.nl
+ * MIT Licensed.
+ */
+const defaultModules = ["alert", "calendar", "clock", "compliments", "newsfeed", "updatenotification", "weather"];
+
+/*************** DO NOT EDIT THE LINE BELOW ***************/
+if (typeof module !== "undefined") {
+	module.exports = defaultModules;
+}
