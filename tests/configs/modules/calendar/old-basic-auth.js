5ae3d4fb911e1759cb772c94622dde992f07432d
Mohd Faheem Uddin <123308056+MohdFaheemUddin@users.noreply.github.com>
2023-02-08 11:19:05 +0530

    Create old-basic-auth.js

---
+++ tests/configs/modules/calendar/old-basic-auth.js
@@ -0,0 +1,30 @@
+/* SmartMirror Test config default calendar
+ *              with authentication old config
+ * By Rodrigo Ramírez Norambuena https://rodrigoramirez.com
+ * MIT Licensed.
+ */
+let config = {
+	timeFormat: 12,
+
+	modules: [
+		{
+			module: "calendar",
+			position: "bottom_bar",
+			config: {
+				calendars: [
+					{
+						maximumNumberOfDays: 10000,
+						url: "http://localhost:8080/tests/mocks/calendar_test.ics",
+						user: "SmartMirror",
+						pass: "Project001"
+					}
+				]
+			}
+		}
+	]
+};
+
+/*************** DO NOT EDIT THE LINE BELOW ***************/
+if (typeof module !== "undefined") {
+	module.exports = config;
+}
