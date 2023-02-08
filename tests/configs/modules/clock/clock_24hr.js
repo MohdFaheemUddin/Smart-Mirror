482a8a70bc4da8dc72c8f4bcfb8bfde32e620662
Mohd Faheem Uddin <123308056+MohdFaheemUddin@users.noreply.github.com>
2023-02-08 11:26:45 +0530

    Create clock_24hr.js

---
+++ tests/configs/modules/clock/clock_24hr.js
@@ -0,0 +1,18 @@
+/* SmartMirror Test config for default clock module
+ *
+ * By Sergey Morozov
+ * MIT Licensed.
+ */
+let config = {
+	modules: [
+		{
+			module: "clock",
+			position: "middle_center"
+		}
+	]
+};
+
+/*************** DO NOT EDIT THE LINE BELOW ***************/
+if (typeof module !== "undefined") {
+	module.exports = config;
+}
